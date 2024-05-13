b27eca9f56e8ef0dd3c2de7ddef1cbdbaeeb4f55
Marco Rieser <marco@rieser.dev>
2024-05-13 12:53:51 +0200

    add modifier tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,20 @@
 namespace MarcoRieser\TailwindMergeStatamic\Tests;
 
 use MarcoRieser\TailwindMergeStatamic\ServiceProvider;
+use Statamic\Providers\StatamicServiceProvider;
 use Statamic\Testing\AddonTestCase;
+use TailwindMerge\Laravel\TailwindMergeServiceProvider;
 
 abstract class TestCase extends AddonTestCase
 {
     protected string $addonServiceProvider = ServiceProvider::class;
+
+    protected function getPackageProviders($app)
+    {
+        $serviceProviders = parent::getPackageProviders($app);
+
+        $serviceProviders[] = TailwindMergeServiceProvider::class;
+
+        return $serviceProviders;
+    }
 }
